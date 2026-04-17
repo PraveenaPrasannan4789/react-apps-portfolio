ef13d48ef99d125d63d14f93d46b8536025c3d5a
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-04-17 13:49:38 +0530

    added media queries example

---
+++ src/components/Media.jsx
@@ -0,0 +1,10 @@
+import "../styles/media.css";
+const Media = () => {
+  return (
+    <div className="main-section">
+      <h1>Media Queries example</h1>
+      <button>Click here</button>
+    </div>
+  );
+};
+export default Media;
